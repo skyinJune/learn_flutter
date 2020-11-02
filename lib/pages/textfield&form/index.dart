6871286460e8275bca32a8b6e6fd1e3d9db15f0c
mazhiwen <mazhiwen@baidu.com>
2020-11-03 00:49:41 +0800

    textField & form practice

--- lib/pages/textfield&form/index.dart
+++ lib/pages/textfield&form/index.dart
@@ -6,6 +6,27 @@ class TextFieldForm extends StatefulWidget {
 }
 
 class _TextFieldForm extends State<TextFieldForm> {
+  TextEditingController _unameController = TextEditingController();
+  TextEditingController _upaswordController = TextEditingController();
+
+  FocusNode _unameFocusNode = FocusNode();
+  FocusNode _upaswordFocusNode = FocusNode();
+
+  GlobalKey _formKey = new GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    // 可以用controll的方式控制输入的内容
+    _unameController.addListener(() {
+      print('uname input :' + _unameController.text);
+    });
+
+    _unameController.text = '用户12345';
+    _unameController.selection = TextSelection(
+        baseOffset: 2, extentOffset: _unameController.text.length);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,24 +34,72 @@ class _TextFieldForm extends State<TextFieldForm> {
         title: Text('textfield & form page'),
       ),
       body: Center(
+          child: Form(
+        key: _formKey,
+        autovalidate: true,
         child: Column(
           children: <Widget>[
-            TextField(
+            TextFormField(
               autofocus: true,
               decoration: InputDecoration(
                   labelText: '用户名',
                   hintText: '手机号或邮箱',
                   prefixIcon: Icon(Icons.person)),
+              controller: _unameController,
+              onChanged: (value) {
+                // print('uname: ' + value);
+              },
+              focusNode: _unameFocusNode,
+              validator: (v) {
+                return v.trim().length > 0 ? null : '用户名不能为空';
+              },
             ),
-            TextField(
-                decoration: InputDecoration(
-                    labelText: '密码',
-                    hintText: '您的登录密码',
-                    prefixIcon: Icon(Icons.lock)),
-                obscureText: true)
+            TextFormField(
+              decoration: InputDecoration(
+                  labelText: '密码',
+                  hintText: '您的登录密码',
+                  prefixIcon: Icon(Icons.lock)),
+              controller: _upaswordController,
+              onChanged: (value) {
+                // onChange也可以用来监听变化
+                print('upasword: ' + value);
+              },
+              obscureText: true,
+              focusNode: _upaswordFocusNode,
+              validator: (v) {
+                return v.trim().length > 5 ? null : '密码不能少于6位';
+              },
+            ),
+            RaisedButton(
+                child: Text('移动焦点'),
+                onPressed: () {
+                  FocusScope.of(context).requestFocus(_upaswordFocusNode);
+                }),
+            RaisedButton(
+                child: Text('隐藏键盘'),
+                onPressed: () {
+                  _unameFocusNode.unfocus();
+                  _upaswordFocusNode.unfocus();
+                }),
+            FlatButton(
+              child: Text('提交'),
+              onPressed: () {
+                if ((_formKey.currentState as FormState).validate()) {
+                  print('验证通过');
+                } else {
+                  print('验证失败');
+                }
+              },
+              color: Colors.blue,
+              highlightColor: Colors.blue[700],
+              colorBrightness: Brightness.dark,
+              splashColor: Colors.grey,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+            )
           ],
         ),
-      ),
+      )),
     );
   }
 }
