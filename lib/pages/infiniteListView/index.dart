5ad2d54f9a8d845cdb02b37568ed96ed181fbc4b
skyinJune <215933471@qq.com>
2020-11-11 23:56:16 +0800

    infinite list add title

--- lib/pages/infiniteListView/index.dart
+++ lib/pages/infiniteListView/index.dart
@@ -26,37 +26,44 @@ class _InfiniteListViewState extends State<InfiniteListView> {
       appBar: AppBar(
         title: Text('infinite listview'),
       ),
-      body: ListView.separated(
-        itemCount: wordList.length,
-        itemBuilder: (BuildContext context, int index) {
-          if (wordList[index] == loadingTag) {
-              if (wordList.length - 1 < 101) {
-                getMoreData();
-                return Container(
-                  padding: EdgeInsets.all(16),
-                  alignment: Alignment.center,
-                  child: SizedBox(
-                    width: 24,
-                    height: 24,
-                    child: CircularProgressIndicator(strokeWidth: 2,),
-                  ),
-                );
-              } else {
-                return Container(
-                  alignment: Alignment.center,
-                  padding: EdgeInsets.all(16),
-                  child: Text(
-                      '已经到底啦~',
-                      style: TextStyle(color: Colors.grey)
-                  ),
-                );
-              }
-          } else {
-            return ListTile(title: Text(wordList[index]));
-          }
-        },
-        separatorBuilder: (context, index) => Divider(height: .0),
-      ),
+      body: Column(
+        children: <Widget>[
+          ListTile(title: Text('标题')),
+          Expanded(
+            child: ListView.separated(
+              itemCount: wordList.length,
+              itemBuilder: (BuildContext context, int index) {
+                if (wordList[index] == loadingTag) {
+                  if (wordList.length - 1 < 101) {
+                    getMoreData();
+                    return Container(
+                      padding: EdgeInsets.all(16),
+                      alignment: Alignment.center,
+                      child: SizedBox(
+                        width: 24,
+                        height: 24,
+                        child: CircularProgressIndicator(strokeWidth: 2,),
+                      ),
+                    );
+                  } else {
+                    return Container(
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(16),
+                      child: Text(
+                          '已经到底啦~',
+                          style: TextStyle(color: Colors.grey)
+                      ),
+                    );
+                  }
+                } else {
+                  return ListTile(title: Text(wordList[index]));
+                }
+              },
+              separatorBuilder: (context, index) => Divider(height: .0),
+            ),
+          )
+        ],
+      )
     );
   }
 }
