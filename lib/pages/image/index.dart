ff30f4b82897193f9a784ed141b07c8ea131555a
skyinJune <215933471@qq.com>
2021-07-05 23:05:55 +0800

    'fadeinImage'

--- lib/pages/image/index.dart
+++ lib/pages/image/index.dart
@@ -16,14 +16,20 @@ class ImagePage extends StatelessWidget {
               'images/avatar.jpeg',
               width: 100,
             ),
-            // Image.network(
-            //   'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603004652089&di=1269e91b3abfeec1a02f448d5fc629f4&imgtype=0&src=http%3A%2F%2Fb.hiphotos.baidu.com%2Fzhidao%2Fwh%253D450%252C600%2Fsign%3Da587b23df11f3a295a9dddcaac159007%2F500fd9f9d72a60590cfef2f92934349b023bba62.jpg',
-            //   width: 100,
-            // ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.network(
+                'http://tiebapic.baidu.com/forum/w%3D580%3B/sign=974e7e2540950a7b75354ecc3aea63d9/bf096b63f6246b60d7759767b6f81a4c510fa2ae.jpg',
+                width: 200,
+              ),
+            ),
             Icon(
               Icons.fingerprint,
               color: Colors.green,
-            )
+            ),
+            FadeInImage.assetNetwork(
+                placeholder: 'images/avatar.jpeg',
+                image: 'https://gameplus-platform.cdn.bcebos.com/gameplus-platform/upload/file/source/ed86296e7704ca7d48fcfafc118cb6aa.jpeg')
           ],
         ),
       ),
