85626bbf412fb24f4f311e2316e05bd2d22b740b
skyinJune <215933471@qq.com>
2021-07-06 21:08:33 +0800

    text各种样式处理

--- lib/pages/text/index.dart
+++ lib/pages/text/index.dart
@@ -1,26 +1,29 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class TextPage extends StatelessWidget {
   TextPage();
   @override
   Widget build(BuildContext context) {
+    GestureRecognizer _recognizer = new TapGestureRecognizer();
     return Scaffold(
       appBar: AppBar(title: Text('text page')),
       body: Container(
         child: Column(
           children: <Widget>[
             Text(
-              'text page',
+              '大字号文字',
               style: TextStyle(fontSize: 24),
             ),
-            Text('text with right align ' * 15, textAlign: TextAlign.right),
+            Text(' 测试一下文本换行 ' * 10),
+            Text(' 居右文本 ' * 15, textAlign: TextAlign.right),
             Text(
-              'one line content => ' * 10,
+              '一行截断' * 10,
               maxLines: 1,
               overflow: TextOverflow.ellipsis,
             ),
             Text(
-              'hello world',
+              '带样式的文字',
               style: TextStyle(
                   fontSize: 18,
                   height: 1.2,
@@ -32,14 +35,21 @@ class TextPage extends StatelessWidget {
             DefaultTextStyle(
               style: TextStyle(fontSize: 20, color: Colors.red),
               child: Column(children: <Widget>[
-                Text('hello world'),
-                Text('I am xxx'),
+                Text('使用外层定义样式 (继承)'),
+                Text('使用外层定义样式 (继承) 2'),
                 Text(
-                  'special style',
+                  '使用特殊样式 (非继承)',
                   style: TextStyle(inherit: false, color: Colors.grey),
                 )
               ]),
             ),
+            Text.rich(TextSpan(children: [
+              TextSpan(text: '使用TextSpan 链接地址：'),
+              TextSpan(
+                  text: 'https://flutterchina.club',
+                  style: TextStyle(color: Colors.blue),
+                  recognizer: _recognizer),
+            ]))
           ],
         ),
       ),
